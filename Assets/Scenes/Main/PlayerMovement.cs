de0c7fb19ee19f733e112a7c19d8b52f28610d1c
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2024-12-13 17:22:19 +0100

    Kaputten Müll entfernt

--- Assets/Scenes/Main/PlayerMovement.cs
+++ Assets/Scenes/Main/PlayerMovement.cs
@@ -1,14 +1,7 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float movement_speed = 25;
-    public Vector2 mouse_sensitivity = new(1, 1);
-
-    Vector2 RelativeMouse() => new(Screen.height / 2 - Input.mousePosition.x, Screen.width / 2 - Input.mousePosition.y);
-
-    void Bewegen(Vector3 wo) {
-        transform.position += Time.deltaTime * movement_speed * wo;
-    }
+    public float MovementSpeed = .07F;
 
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
@@ -17,13 +10,12 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update() {
         // Movement
-        Vector3 nach(Vector3 wo) => transform.rotation * wo;
-        int taste(KeyCode key) => Input.GetKey(key) ? 1 : 0;
+        static int taste(KeyCode key) => Input.GetKey(key) ? 1 : 0;
 
-        Bewegen(nach(Vector3.forward) * (taste(KeyCode.W) - taste(KeyCode.S)));
-        Bewegen(nach(Vector3.left) * (taste(KeyCode.A) - taste(KeyCode.D)));
+        Vector3 vorne = new(Vector3.forward.x, 0, Vector3.forward.z);
+        Vector3 links = new(Vector3.left.x, 0, Vector3.left.z);
 
-        // Rotation
-        transform.rotation = Quaternion.AngleAxis(RelativeMouse().y * mouse_sensitivity.y, new(1, 0, 0));
+        transform.Translate(transform.rotation * vorne * (taste(KeyCode.W) - taste(KeyCode.S)) * MovementSpeed);
+        transform.Translate(transform.rotation * links * (taste(KeyCode.A) - taste(KeyCode.D)) * MovementSpeed);
     }
 }
