93fb97b51687cf760290b4788f558c4e186d8b28
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2024-12-13 17:34:14 +0100

    Very interesting rotation

--- Assets/Scenes/Main/PlayerMovement.cs
+++ Assets/Scenes/Main/PlayerMovement.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float MovementSpeed = .07F;
 
+    public Vector2 MouseSensitivity = Vector2.one;
+
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update() {
-        // Movement
         static int taste(KeyCode key) => Input.GetKey(key) ? 1 : 0;
 
         Vector3 vorne = new(Vector3.forward.x, 0, Vector3.forward.z);
@@ -17,5 +18,11 @@ public class PlayerMovement : MonoBehaviour {
 
         transform.Translate(transform.rotation * vorne * (taste(KeyCode.W) - taste(KeyCode.S)) * MovementSpeed);
         transform.Translate(transform.rotation * links * (taste(KeyCode.A) - taste(KeyCode.D)) * MovementSpeed);
+
+        float yaw = Input.GetAxis("Mouse X") * MouseSensitivity.x;
+        float pitch = Input.GetAxis("Mouse Y") * MouseSensitivity.y;
+        Vector3 rotation = new(yaw, pitch);
+
+        transform.Rotate(rotation, Space.World);
     }
 }
