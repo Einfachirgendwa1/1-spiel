e94d1df4e9b8de2757b3fa8d8a4c609bac09bde4
Hackerman42069-de <timkoerth1@gmail.com>
2024-12-14 23:06:14 +0100

    s

--- Assets/Scripts/WeaponSwitching.cs
+++ Assets/Scripts/WeaponSwitching.cs
@@ -1,16 +1,71 @@
+using System;
 using UnityEngine;
 
 public class WeaponSwitching : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private Transform[] weapons;
+
+    [SerializeField] private KeyCode[] keys;
+
+    [SerializeField] private float switchTime;
+
+    private int selectedWeapon;
+    private float timeSinceLastSwitch;
+
+    private void SetWeapons()
+    {
+        weapons = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            weapons[i] = transform.GetChild(i);
+        }
+
+        if (keys == null) keys = new KeyCode[weapons.Length];
+    }
+
+    private void Select(int weaponIndex)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].gameObject.SetActive(i == weaponIndex);
+        }
+
+        timeSinceLastSwitch = 0f;
+
+        OnWeaponSelected();
+    }
+
+    private void OnWeaponSelected()
+    {
+        print("Selected new Weapon");
+    }
+
     void Start()
     {
-        
+        SetWeapons();
+        Select(selectedWeapon);
+
+        timeSinceLastSwitch = 0f;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        int previousSelectedWeapon = selectedWeapon;
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            if (Input.GetKeyDown(keys[i]) && timeSinceLastSwitch >= switchTime)
+            {
+                selectedWeapon = i;
+            }
+
+            if (previousSelectedWeapon != selectedWeapon) Select(selectedWeapon);
+
+            timeSinceLastSwitch += Time.deltaTime;
+        }
     }
 }
