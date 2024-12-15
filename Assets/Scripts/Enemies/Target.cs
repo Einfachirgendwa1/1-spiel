2da2818dd7b39d85a499b8034145047dad975c21
Hackerman42069-de <timkoerth1@gmail.com>
2024-12-15 13:04:05 +0100

    s

--- Assets/Scripts/Enemies/Target.cs
+++ Assets/Scripts/Enemies/Target.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class Target : MonoBehaviour, IDamageable
 {
     private float health = 100f;
-
+    [SerializeField] private AudioSource audioSource1;
+    [SerializeField] private AudioSource audioSource2;
     public void TakeDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
             Destroy(gameObject);
+            audioSource2.Play();
         }
+        audioSource1.Play();
     }
     
 }
