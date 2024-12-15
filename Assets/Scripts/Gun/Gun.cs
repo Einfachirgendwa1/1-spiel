2892aca8e353fb75fbec327da821a4283c258c38
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2024-12-15 11:58:17 +0100

    ich habe ganz viele Fragen

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,11 +1,8 @@
-using System;
 using System.Collections;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-
+public class Gun : MonoBehaviour {
     [SerializeField] GunData gunData;
     [SerializeField] Transform cam;
 
@@ -18,12 +15,13 @@ public class Gun : MonoBehaviour
     public AudioClip reloadSound;
 
     float timeSinceLastShot;
+    // Warum brauchen wir das? Spezifiziert die Gun nicht die fireRate?
     float fireRate = 1.0f;
 
     public ParticleSystem muzzleFlash;
 
-    void Start()
-    {
+    void Start() {
+        // Das wirkt viel zu kompliziert für das, was wir machen wollen
         PlayerShoot.shootInput += Shoot;
         PlayerShoot.reloadInput += StartReload;
 
@@ -31,20 +29,20 @@ public class Gun : MonoBehaviour
         pistolAnimator = GetComponent<Animator>();
     }
 
+    // Warum ist das wichtig?
     private void OnDisable() => gunData.reloading = false;
 
-    public void StartReload()
-    {
-        if (!gunData.reloading && this.gameObject.activeSelf)
-        {
+    public void StartReload() {
+        // Was macht activeSelf?
+        if (!gunData.reloading && gameObject.activeSelf) {
             StartCoroutine(Reload());
             pistolAnimator.SetTrigger("Reload");
             gunAudio.PlayOneShot(reloadSound, 1);
         }
     }
 
-    private IEnumerator Reload()
-    {
+    // Müssen wir das über eine Coroutine machen oder können wir einfach WaitForSeconds(gunData.reloadTime) am Ende haben?
+    private IEnumerator Reload() {
         gunData.reloading = true;
 
         yield return new WaitForSeconds(gunData.reloadTime);
@@ -53,15 +51,14 @@ public class Gun : MonoBehaviour
 
         gunData.reloading = false;
     }
+
+    // Warum fireRate / ...?
     private bool CanShoot() => !gunData.reloading && timeSinceLastShot > fireRate / (gunData.fireRate / 60f);
-    private void Shoot()
-    {
-        if (gunData.currentAmmo > 0)
-        {
-            if (CanShoot())
-            {
-                if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance))
-                {
+    private void Shoot() {
+        if (gunData.currentAmmo > 0) {
+            if (CanShoot()) {
+                // Was zum Teufel macht out RaycastHit hitInfo?
+                if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance)) {
                     IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
                     damageable?.TakeDamage(gunData.damage);
                 }
@@ -72,21 +69,15 @@ public class Gun : MonoBehaviour
 
                 gunData.currentAmmo--;
                 timeSinceLastShot = 0;
-                OnGunShot();
             }
         }
     }
 
-    
-    void Update()
-    {
+
+    void Update() {
+        // Könnten wir timeSinceLastShot nicht wie Reload über eine Coroutine machen?
         timeSinceLastShot += Time.deltaTime;
 
         ammoText.SetText("Ammo: " + gunData.currentAmmo);
-    }
-
-    private void OnGunShot()
-    {
-
     }
 }
