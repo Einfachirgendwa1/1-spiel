9725940de893b8b34bccc5a32d700f515790353b
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-01-13 18:38:40 +0100

    slower in air

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,35 +3,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     [Header("Movement")]
     public float movementSpeed = 10;
+    public float movementSpeedMultiplier = 500;
     public float jumpForce = 400;
     public float movePercentageGround = 100;
     public float movePercentageAir = 30;
-
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode crouchKey = KeyCode.C;
 
     [Header("Collision")]
     public LayerMask whatIsGround;
 
 
     Rigidbody playerRb;
-    CapsuleCollider capsuleCollider;
 
     private void Start() {
         playerRb = GetComponent<Rigidbody>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-
-        playerRb.linearDamping = 6f;
     }
 
     private void FixedUpdate() {
-        Physics.Raycast(transform.position, Vector3.down, out RaycastHit raycasthit, 1.5F, whatIsGround);
+        bool grounded = Physics.Raycast(transform.position, Vector3.down, out RaycastHit raycasthit, 1.5F, whatIsGround);
         Vector3 plane = raycasthit.normal;
 
         Vector3 moveDirection = Vector3.forward * Input.GetAxisRaw("Vertical") + Vector3.right * Input.GetAxisRaw("Horizontal");
         Vector3 direction = (transform.rotation * moveDirection).normalized;
 
-        playerRb.AddForce(Vector3.ProjectOnPlane(direction, plane) * movementSpeed * 500, ForceMode.Force);
+        if (grounded) {
+            movementSpeedMultiplier = 1000;
+        } else {
+            movementSpeedMultiplier = 1000 / 3;
+        }
+
+        playerRb.AddForce(movementSpeed * movementSpeedMultiplier * Vector3.ProjectOnPlane(direction, plane), ForceMode.Force);
     }
 }
