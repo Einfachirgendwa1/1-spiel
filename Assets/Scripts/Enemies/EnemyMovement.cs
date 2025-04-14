c4c457e9c0f4261600187310940f29936cc441e3
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-04-14 21:40:06 +0200

    forgor smth

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,37 +1,9 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Assertions;
 
 namespace Enemies {
-    [Serializable]
-    public class Path {
-        public static Path empty = new(new Vector3[] { });
-        private readonly Vector3[] points;
-        private int index;
-
-        public Path(Vector3[] points) {
-            this.points = points;
-            index = 0;
-        }
-
-        /// <summary>
-        ///     Returns the next point in the path.
-        /// </summary>
-        /// <returns>Next point in the path or null if the path has no points.</returns>
-        public Vector3? NextPoint() {
-            if (points.Length == 0) {
-                return null;
-            }
-
-            index = index++ % points.Length;
-            return points[index];
-        }
-    }
-
     public class EnemyMovement : MonoBehaviour {
-        public Path patrollingPath = Path.empty;
-
         private NavMeshAgent agent;
         private EnemyPlayerDetection detection;
         private Vector3? target;
@@ -47,27 +19,14 @@ namespace Enemies {
 
         private Vector3 GetTargetPoint() {
             return detection.state switch {
-                EnemyState.Patrolling => FollowPath(patrollingPath),
+                EnemyState.Patrolling => FollowPath(),
                 EnemyState.Attacking  => AttackPlayer(),
                 _                     => throw new ArgumentOutOfRangeException()
             };
         }
 
-        private Vector3 FollowPath(Path path) {
-            target ??= path.NextPoint();
-
-            // we have no points to go to, so we just stand there doing nothing
-            if (target == null) {
-                return transform.position;
-            }
-
-            Vector3 distance = target.Value - transform.position;
-            if (distance.magnitude < 0.1f) {
-                target = path.NextPoint();
-                Assert.IsTrue(target.HasValue);
-            }
-
-            return target.Value;
+        private Vector3 FollowPath() {
+            return Vector3.zero;
         }
 
         private Vector3 AttackPlayer() {
