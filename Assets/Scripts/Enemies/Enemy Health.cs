55efe51873bd465248e55c3b7f10f1bc90f37080
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-05-10 22:23:12 +0200

    fix enemy health

--- Assets/Scripts/Enemies/Enemy Health.cs
+++ Assets/Scripts/Enemies/Enemy Health.cs
@@ -1,36 +1,29 @@
-using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour {
-    public float enemyHealth = 100;
-    [SerializeField] float timeToWait = 1000;
+namespace Enemies {
+    public class EnemyHealth : MonoBehaviour {
+        public float enemyHealth = 100;
+        [SerializeField] private float timeToWait = 1;
+        public AudioClip deathSound;
 
-    AudioSource enemyAudio;
-    public AudioClip deathSound;
+        private AudioSource enemyAudio;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-        enemyAudio = GetComponent<AudioSource>();
-    }
+        private void Start() {
+            enemyAudio = GetComponent<AudioSource>();
+        }
 
-    // Update is called once per frame
-    void Update() {
+        public void GetDamage(float damage) {
+            enemyHealth -= damage;
+            if (enemyHealth <= 0) {
+                StartCoroutine(Die());
+            }
+        }
 
-    }
-
-    public void GetDamage(float damage) {
-        enemyHealth -= damage;
-        if (enemyHealth <= 0) {
-            Die();
+        private IEnumerator Die() {
+            enemyAudio.PlayOneShot(deathSound);
+            yield return new WaitForSeconds(timeToWait);
+            Destroy(gameObject);
         }
     }
-
-    public async void Die() {
-        enemyAudio.PlayOneShot(deathSound);
-        //Death animation spielen
-        //Task.Delay sorgt für warten d.h. enemy bleibt liegen
-        await Task.Delay(3000);
-        GameObject.Destroy(gameObject);
-    }
-
 }
