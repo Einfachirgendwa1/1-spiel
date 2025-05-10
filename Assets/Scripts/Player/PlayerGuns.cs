264e8f05c533e2506bb283d5676752557a24537e
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-05-10 20:09:27 +0200

    forogor

--- Assets/Scripts/Player/PlayerGuns.cs
+++ Assets/Scripts/Player/PlayerGuns.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 namespace Player {
     public class PlayerGuns : Guns {
         public void Update() {
-            Debug.Log(guns[0].Ammo);
-
-            if (Input.GetKey(KeyCode.F)) {
-                CurrentGun.gameObject.SetActive(!CurrentGun.gameObject.activeSelf);
-            }
-
             for (KeyCode key = KeyCode.Alpha1; key <= KeyCode.Alpha9; key++) {
                 if (Input.GetKeyDown(key)) {
                     SelectGun(key - KeyCode.Alpha1);
