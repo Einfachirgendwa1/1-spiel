6f246e48c8d36f8a7ed2e200fccb0ef715b71e08
Robicode379 <191622328+Robicode379@users.noreply.github.com>
2025-12-16 15:09:47 +0100

    simples tür öffnugs system

---
+++ Assets/Scripts/Interaction/DoorOpen.cs
@@ -0,0 +1,37 @@
+using Interaction;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private bool isOpen = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    public string Description => "move door";
+
+    public bool CanInteract => true;
+
+    public void Interact()
+    {
+        if (!isOpen)
+        {
+
+            transform.Rotate(0, 0, 90);
+            isOpen = true;
+        }
+        else
+        {
+            transform.Rotate(0, 0, -90);
+            isOpen = false;
+        }
+
+        }
+    }
+
+
+    
