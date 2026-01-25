6f7b3fa6ac465ac76b9dc3b139cd75f0ba7b0c75
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-01-25 02:03:37 +0100

    just forgot like half of the refactor

--- Assets/Scripts/Guns/Gun.cs
+++ Assets/Scripts/Guns/Gun.cs
@@ -68,12 +68,7 @@ namespace Guns {
             Quaternion weaponSpray = Quaternion.AngleAxis(x, Vector3.up) * Quaternion.AngleAxis(y, Vector3.right);
             Quaternion recoilOffset = Quaternion.AngleAxis(shotsInARow * recoil, Vector3.right);
 
-            Vector3 camPos = Controller.cam.transform.position;
-            Vector3 toTarget = Physics.Raycast(camPos, Controller.cam.transform.forward, out RaycastHit crosshairHit)
-                ? (crosshairHit.point - transform.position).normalized
-                : Controller.cam.transform.forward;
-
-            Vector3 direction = weaponSpray * recoilOffset * toTarget;
+            Vector3 direction = weaponSpray * recoilOffset * DirectionToCrosshairTarget();
 
             if (Physics.Raycast(transform.position, direction, out RaycastHit hit, range)) {
                 hit.transform.GetComponent<ITarget>()?.TakeDamage(damage);
