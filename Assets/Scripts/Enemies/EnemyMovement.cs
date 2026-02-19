c9ee8dc5cc49f7c7c958ae9eab386ecf549fec46
Robicode379 <191622328+Robicode379@users.noreply.github.com>
2026-02-19 21:27:25 +0100

    Enemy walk animation

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,21 +1,32 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Animations;
+using UnityEngine;
 using UnityEngine.AI;
 using Validation;
 
 namespace Enemies {
     public class EnemyMovement : MonoBehaviour {
+
         private static readonly int Speed = Animator.StringToHash("Speed");
 
-        [NonNull] public Animator animator;
+        [NonNull] public Animator enemyAnim;
         [NonNull] public NavMeshAgent agent;
+        public float maxSpeed = 5;
+        internal float currentSpeed;
 
         internal Vector3 Destination;
 
+        public void Start()
+        {
+            enemyAnim = GetComponent<Animator>();
+            agent = GetComponent<NavMeshAgent>();
+        }
         private void Update() {
             agent.destination = Destination;
+            agent.speed = currentSpeed;
+           
 
             if (agent.velocity.magnitude > 0.1f) {
-                animator.SetFloat(Speed, agent.velocity.magnitude);
+                enemyAnim.SetFloat(Speed, agent.velocity.magnitude/maxSpeed);
             }
         }
     }
--- Assets/Scripts/Enemies/Patrolling.cs
+++ Assets/Scripts/Enemies/Patrolling.cs
@@ -8,6 +8,7 @@ namespace Enemies {
         [NonNull] public EnemyMovement movement;
         [NonNull] public Attacking attacking;
         [NonNull] public Alerted alerted;
+       
 
         internal readonly List<Vector3> PatrollingPath = new();
         private int patrollingPathHead;
@@ -17,10 +18,12 @@ namespace Enemies {
             if (PatrollingPath.Count != 0) {
                 target = PatrollingPath[0];
             }
+      
         }
 
         [Preserve]
         public void WhenPatrolling() {
+            movement.currentSpeed = 2;
             if (attacking.CanSeePlayer()) {
                 alerted.Alert(5);
             }
