7b6ea75c7730d7988edb077710fd099968724cff
Robicode379 <191622328+Robicode379@users.noreply.github.com>
2026-05-10 16:00:38 +0200

    worjing doors

--- Assets/Scripts/Interaction/DoorOpen.cs
+++ Assets/Scripts/Interaction/DoorOpen.cs
@@ -1,21 +1,43 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Interaction {
     public class DoorOpen : MonoBehaviour, IInteractable {
-        public bool isOpen;
+        
+        [SerializeField]
+        private bool isOpen;
+        [SerializeField]
+        private bool leftWing;
+        [Range(-1, 1)]
+        private int turnMultiplier;
 
+        private GameObject pivot; 
+
+        public void Start()
+        {
+            pivot = this.transform.parent.gameObject;
+
+            if (leftWing)
+            {
+                turnMultiplier = -1;
+            }
+            else
+            {
+                turnMultiplier = 1; 
+            }
+        }
         public string Description => "move door";
 
         public bool CanInteract => true;
 
         public void Interact() {
             if (!isOpen) {
-                transform.Rotate(0, 0, 90);
+                pivot.transform.Rotate(0, 0, turnMultiplier * 90);
                 isOpen = true;
             } else {
-                transform.Rotate(0, 0, -90);
+                pivot.transform.Rotate(0, 0, turnMultiplier * -90);
                 isOpen = false;
             }
-        }
+        }  
     }
 }
